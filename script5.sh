3f738a38dc0a4c3b4d4ba6e548d919823aa472e5
trongpb <166879317+trongpb@users.noreply.github.com>
2025-07-24 22:42:03 +0700

    Add files via upload

---
+++ script5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+ mlr --icsv --opprint put'  
+ $profit= ($revenue - $budget) ;
+  $original_title = string($original_title);
+$Month =   int(index($release_date, "/") == 2 ? substr($release_date, 0, 0) : substr($release_date, 0, 1));
+$Day = int(index($release_date, "/") == 2  
+?
+(index(substr($release_date,2,strlen($release_date)-1), "/") == 2 ? substr(substr($release_date,2,strlen($release_date)-2), 0, 0) : substr(substr($release_date,2,strlen($release_date)-2), 0, 1))
+: 
+(index(substr($release_date,3,strlen($release_date)-2), "/") == 2 ? substr(substr($release_date,3,strlen($release_date)-2), 0, 0) : substr(substr($release_date,3,strlen($release_date)-2), 0, 1)));
+' then  put '$release_year = int($release_year)'   \
+  then  filter '$id !=""  && is_numeric($id) && is_numeric($Month) && substr($imdb_id,0,1) == "tt"  && $imdb_id !="" &&  
+  is_numeric($popularity) && $popularity != ""  &&  is_numeric($budget) && $budget != "" &&  is_numeric($revenue) && 
+  $revenue >0 && is_string($original_title)  && $original_title !="" &&
+  ($original_title  !=~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$") && is_string($cast)  && 
+  $cast !=""  && ($homepage == "" || $homepage =~ "^https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/.*)?$") &&
+  is_string($director)  && $director !="" &&  is_string($tagline) &&  is_string($keywords)  &&
+  is_string($overview)  &&   is_numeric($runtime)  && $runtime !="" &&   is_string($keywords)  &&
+  is_string($genres)  && is_string($production_companies)  && $production_companies !="" && 
+  is_numeric($vote_count)  && is_numeric($vote_average)  &&  $vote_average !="" &&  $vote_average != null &&
+  is_numeric($budget_adj)  && $budget_adj !="" && $budget_adj >=0 && is_numeric($revenue_adj)  && $revenue_adj !=""   && 
+  $revenue_adj >=0 &&  $release_year =~ "^[0-9]{4}$" &&
+  ($release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$" || $release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{2}$")' \
+   then sort -nr profit tmdb-movies.csv | head -n 11
---
+++ script6.sh
@@ -0,0 +1,36 @@
+#!/bin/bash  
+mlr --icsv --opprint \
+filter '$id !=""  && is_numeric($id) &&  substr($imdb_id,0,1) == "tt"  && $imdb_id !="" &&  
+  is_numeric($popularity) && $popularity != ""  &&  is_numeric($budget) && $budget != "" &&  is_numeric($revenue) && 
+  $revenue >0 && is_string($original_title)  && $original_title !="" &&
+  ($original_title  !=~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$") && is_string($cast)  && 
+  $cast !=""  && ($homepage == "" || $homepage =~ "^https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/.*)?$") &&
+  (is_string($director)  && $director !="" && $original_title  !=~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$") &&  is_string($tagline) &&  is_string($keywords)  &&
+  is_string($overview)  &&   is_numeric($runtime)  && $runtime !="" &&   is_string($keywords)  &&
+  is_string($genres) && $genres !="" && is_string($production_companies)  && $production_companies !="" && 
+  is_numeric($vote_count)  && is_numeric($vote_average)  &&  $vote_average !="" &&  $vote_average != null &&
+  is_numeric($budget_adj)  && $budget_adj !="" && $budget_adj >=0 && is_numeric($revenue_adj)  && $revenue_adj !=""   && 
+  $revenue_adj >=0 &&  $release_year =~ "^[0-9]{4}$" &&
+  ($release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$" || $release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{2}$")' \
+   then nest --explode --values --across-records -f director --nested-fs '|' \
+   then count -g director   \
+   then sort -nr count \
+   tmdb-movies-goc.csv  | head -n 2
+
+   mlr --icsv --opprint \
+filter '$id !=""  && is_numeric($id) &&  substr($imdb_id,0,1) == "tt"  && $imdb_id !="" &&  
+  is_numeric($popularity) && $popularity != ""  &&  is_numeric($budget) && $budget != "" &&  is_numeric($revenue) && 
+  $revenue >0 && is_string($original_title)  && $original_title !="" &&
+  ($original_title  !=~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$") && is_string($cast)  && 
+  $cast !=""  && ($homepage == "" || $homepage =~ "^https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(/.*)?$") &&
+  (is_string($director)  && $director !="" && $original_title  !=~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2,4}$") &&  is_string($tagline) &&  is_string($keywords)  &&
+  is_string($overview)  &&   is_numeric($runtime)  && $runtime !="" &&   is_string($keywords)  &&
+  is_string($genres) && $genres !="" && is_string($production_companies)  && $production_companies !="" && 
+  is_numeric($vote_count)  && is_numeric($vote_average)  &&  $vote_average !="" &&  $vote_average != null &&
+  is_numeric($budget_adj)  && $budget_adj !="" && $budget_adj >=0 && is_numeric($revenue_adj)  && $revenue_adj !=""   && 
+  $revenue_adj >=0 &&  $release_year =~ "^[0-9]{4}$" &&
+  ($release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{4}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{4}$" || $release_date =~ "^[0-9]{1,2}/[0-9]{1,2}/[0-9]{2}$"  || $release_date =~ "^[0-9]{1,2}-[0-9]{1,2}-[0-9]{2}$")' \
+   then nest --explode --values --across-records -f cast --nested-fs '|' \
+   then count -g cast   \
+   then sort -nr count \
+   tmdb-movies-goc.csv  | head -n 2
